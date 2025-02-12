9e3eaab4f5156c9e033572d89946a6cfa751d5b0
Piotr Wójcik <p.w9714x3@gmail.com>
2025-02-12 13:35:34 +0100

    fix: Removed cache from .gitignore and added missing cache files

---
+++ TagsService/Cache/IRedisCache.cs
@@ -0,0 +1,7 @@
+﻿using TagsService.Models;
+
+public interface IRedisCache
+{
+    Task<IEnumerable<Tag>> GetTagsAsync();
+    Task SetTagsAsync(IEnumerable<Tag> tags);
+}
---
+++ TagsService/Cache/RedisCache.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+using StackExchange.Redis;
+using TagsService.Models;
+
+public class RedisCache : IRedisCache
+{
+    private readonly IDatabase _db;
+    private readonly ILogger<RedisCache> _logger;
+
+    public RedisCache(IConnectionMultiplexer redis, ILogger<RedisCache> logger)
+    {
+        _db = redis.GetDatabase();
+        _logger = logger;
+    }
+
+    public async Task<IEnumerable<Tag>> GetTagsAsync()
+    {
+        var data = await _db.StringGetAsync("tags");
+        return string.IsNullOrEmpty(data) ? new List<Tag>() : JsonConvert.DeserializeObject<List<Tag>>(data);
+    }
+
+    public async Task SetTagsAsync(IEnumerable<Tag> tags)
+    {
+        await _db.StringSetAsync("tags", JsonConvert.SerializeObject(tags), TimeSpan.FromHours(1));
+    }
+}
